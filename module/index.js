1f2b6ee3b86954166b6f6995af386ac5bf11d215
Ambuj <ambujp863@gmail.com>
2024-12-15 19:01:06 +0530

    first commit

---
+++ module/index.js
@@ -0,0 +1,8 @@
+// const fs= require('fs');
+// fs.writeFileSync("module/Hellow.txt","Code step by step");   // create af file 
+
+const gs=require('fs').writeFileSync;
+gs("xyz.txt","abccvb");
+
+
+console.log("-------->",__filename);
